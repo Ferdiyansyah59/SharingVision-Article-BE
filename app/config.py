9906be76376fcad39ce5c7dff6027dda28910865
Ferdiyansyah59 <ferdir5599@gmail.com>
2025-10-21 11:58:57 +0700

    feat: for production

--- app/config.py
+++ app/config.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    DB_HOST = os.getenv('DB_HOST', 'localhost')
+    DB_HOST = os.getenv('DB_HOST', '212.85.24.186')
     DB_USER = os.getenv('DB_USER', 'root')
-    DB_PASSWORD = os.getenv('DB_PASSWORD', 'root')
+    DB_PASSWORD = os.getenv('DB_PASSWORD', 'secretAnI@Ni1924')
     DB_NAME = os.getenv('DB_NAME', 'CDatabase')
-    DB_PORT = os.getenv('DB_PORT', 8889)
+    DB_PORT = os.getenv('DB_PORT', 3306)
